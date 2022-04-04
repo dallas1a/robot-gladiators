85ca4e402ed1de0b71d92267829452cb995dbb7c
dalla@MSI <dallas.archer.bailey@gmail.com>
2022-04-04 17:56:20 -0400

    added console logs and started writing js file

---
+++ assets/js/game.js
@@ -0,0 +1,14 @@
+var playerName = window.prompt("What is your robot's name?");
+window.alert(playerName);
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+//function fight() {
+    
+  
+  //fight(playerName);
+  var name = "your name";
+console.log(name);
