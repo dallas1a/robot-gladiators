d5b0348a0a5c32b92a3f8a324182b9d2ca9ed44f
dalla@MSI <dallas.archer.bailey@gmail.com>
2022-04-05 21:08:09 -0400

    added enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,11 +5,15 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth, playerMoney);
 
-var enemyName = "Roborto";
-var enemyHealth = 40;
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+
+
+
+
+var fight = function(enemyName) {
   window.alert("Welcome to Robot Gladiators!");
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -60,4 +64,6 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   }
   }
 
-fight();
+  for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+  }
