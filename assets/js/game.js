8fa92aeec870c77f2535d7e5fbbbac66a94d3eb4
dalla@MSI <dallas.archer.bailey@gmail.com>
2022-04-06 13:51:53 -0400

    added start and endgame function

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,11 +2,10 @@ var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth, playerMoney);
+
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-var enemyHealth = 10;
+var enemyHealth = 50;
 var enemyAttack = 12;
 
 console.log(enemyNames);
@@ -72,7 +71,10 @@ var fight = function(enemyName) {
       }
       
 };     
-      
+var startGame = function() {    
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
 for(var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
       window.alert("Welcome to Robot Gladiators! Round" + ( i + 1 ) );
@@ -85,3 +87,24 @@ for(var i = 0; i < enemyNames.length; i++) {
     break;
     }
 } 
+endGame();
+}
+startGame();
+var endGame = function() {
+   // if player is still alive, player wins!
+   if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  } 
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+}
+var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  // restart the game
+  startGame();
+} 
+else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+};
